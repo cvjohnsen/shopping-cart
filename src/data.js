f8941491c5f856129ddbb0d55334fa11b6d21d1f
Your Name <cynthiajohnsen@Cynthias-MacBook-Pro-2.local>
2021-04-25 16:25:52 -0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,45 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'Cactus Ball',
+      price: 25,
+      image: '/assets/images/1.jpg'
+    },
+    {
+      id: '2',
+      name: 'Pokey Cactus',
+      price: 45,
+      image: '/assets/images/2.jpg'
+    },
+    {
+      id: '3',
+      name: 'Red Cactus',
+      price: 35,
+      image: '/assets/images/3.jpg'
+    },
+    {
+      id: '4',
+      name: 'Yellow Cactus',
+      price: 35,
+      image: '/assets/images/4.jpg'
+    },
+    {
+      id: '5',
+      name: 'Flower Cactus',
+      price: 45,
+      image: '/assets/images/5.jpg'
+    },
+    {
+      id: '6',
+      name: 'Goddie Cactus',
+      price: 50,
+      image: '/assets/images/6.jpg'
+    },
+  ],
+};
+
+export default data;
+
+
+
